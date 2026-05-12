822cad2d8727eefde3d9ad02b06ccb702ab46ace
Yechan Kang <yckang1124@snu.ac.kr>
2026-05-12 23:40:55 +0900

    .

--- src/components/features/Header.tsx
+++ src/components/features/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
-    <header className="flex h-22 w-full items-center justify-center border-b border-gray-200 bg-white px-8">
-      <h1 className="text-3xl font-bold">FC온라인 강화 시뮬레이터</h1>
+    <header className="flex h-22 w-full items-center justify-center border-gray-200 bg-gray-900 px-8">
+      <h1 className="text-3xl font-bold text-[#02f568]">
+        FC온라인 강화 시뮬레이터
+      </h1>
     </header>
   );
 };
